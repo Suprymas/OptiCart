6adf911631cdf7a92b54702f71ef906b3cdc5f68
Suprymas <macysjustas1@gmail.com>
2026-03-15 12:26:21 +0100

    # KAN-24 / Subtask #KAN-34 / Sukurta naudotojo sąsaja sukurti krepšelio šablonui.

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import search_view, basket_comparison, template_compare
+from .views import search_view, basket_comparison, template_compare, demo_save_template
 
 app_name = 'shop'
 
 urlpatterns = [
     path('', search_view, name='search'),
     path('compare/', basket_comparison, name='compare'),
+    path('compare/demo/', demo_save_template, name='compare_demo'),
     path('template/<int:template_id>/compare/', template_compare, name='template_compare'),
 ]
